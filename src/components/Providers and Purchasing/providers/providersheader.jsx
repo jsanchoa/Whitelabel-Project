d3e4b337cee3cac1794d9f6c58a18dae9d7b5f96
Noelia Segura <156636908+NoeSegura11@users.noreply.github.com>
2025-07-28 23:13:34 -0600

    Providers Header Update

--- src/components/Providers and Purchasing/providers/providersheader.jsx
+++ src/components/Providers and Purchasing/providers/providersheader.jsx
@@ -7,14 +7,14 @@ export const ProvidersHeader = () => {
     console.log("Searching for:", query);
   };
 
-  return (
+   return (
     <>
       <div className="w-full flex justify-center">
         <div className="w-full max-w-screen-xl p-4 pt-6 pb-2">
           <h2 className="text-2xl font-semibold text-neutral-800">Providers</h2>
         </div>
       </div>
-
+      
       <div className="w-full flex justify-center">
         <div className="flex justify-between items-center w-full max-w-screen-xl p-4 pt-0">
           <div className="flex-1">
