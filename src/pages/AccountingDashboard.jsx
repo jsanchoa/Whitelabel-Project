aed1f7aaf9dda75df8069111fe912ff01fbb7b5d
Manuel MM <mmmongecr@hotmail.com>
2025-08-19 11:12:09 -0600

    Accounting

--- src/pages/AccountingDashboard.jsx
+++ src/pages/AccountingDashboard.jsx
@@ -1,53 +1,92 @@
-import React from 'react';
+//import React from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import CustomTable from "@/components/customs/c_simpleTable"
 import AccChart from "@/components/accounting/Acc_Chart"
+import React, { useState, useEffect } from 'react';
 
-const chartData = [
-  { date: "2025-04-01", incomes: 222, expenses: 150 },
-  { date: "2025-04-02", incomes: 97, expenses: 180 },
-  { date: "2025-04-03", incomes: 167, expenses: 120 },
-  { date: "2025-06-30", incomes: 446, expenses: 400 },
-]
+
+
 
 export const AccountingDashboard = () => {
   const [timeRange, setTimeRange] = React.useState("90d")
 
-  const filteredData = chartData.filter((item) => {
-    const date = new Date(item.date)
-    const referenceDate = new Date("2025-06-30")
+
+  //Hook to save my data
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [accountingData, setAccountingData] = useState([]);
+
+  //useEffect funciona para cargar la funcion cada vez que renderiza la pagina
+  useEffect(() => {
+    getIncomesList();
+  }, []);
+
+  useEffect(() => {
+    getExpensesList();
+  }, []);
+  useEffect(() => {
+    getAccountingDataList();
+  }, []);
+
+
+  // Functions to get Incomes and Expenses
+  const getIncomesList = async () => {
+
+    try {
+      // Variable for wait the get response and then save it into categories useState hook
+      const response = await api.get('http://localhost:3000/v1/incomes/list');
+      setIncomes(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+  const getExpensesList = async () => {
+
+    try {
+      // Variable for wait the get response and then save it into categories useState hook
+      const response = await api.get('http://localhost:3000/v1/expenses/list');
+      setExpenses(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+  const getAccountingDataList = async () => {
+
+    try {
+      // Variable for wait the get response and then save it into categories useState hook
+      const response = await api.get('http://localhost:3000/v1/accountingdata/list');
+      setAccountingData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+
+  const chartData = accountingData.filter((item) => {
+    const date = new Date(item.Accounting_Date)
+    const referenceDate = new Date("2025-08-19")
     let daysToSubtract = 90
     if (timeRange === "30d") {
       daysToSubtract = 30
     } else if (timeRange === "7d") {
       daysToSubtract = 7
-    }
-    const startDate = new Date(referenceDate)
-    startDate.setDate(startDate.getDate() - daysToSubtract)
-    return date >= startDate
-  })
-
-  const incomesData = filteredData.map((item) => ({
-    date: item.date,
-    amount: item.incomes,
-  }));
-
-  const expensesData = filteredData.map((item) => ({
-    date: item.date,
-    amount: item.expenses,
-  }));
+  }});
 
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-4">Accounting Dashboard</h1>
-       <p className="mb-6">
-        The Accounting Dashboard provides an overview of your financial data, including incomes and expenses over the selected time period. 
+      <p className="mb-6">
+        The Accounting Dashboard provides an overview of your financial data, including incomes and expenses over the selected time period.
         Use the time filters to adjust the range and view detailed tables and charts for better financial analysis.
       </p>
       <div className="grid grid-cols-1 gap-6">
-        
+
         <div className="mt-6">
-          <AccChart data={filteredData} />
+          <AccChart data={accountingData} />
         </div>
 
         <div className="flex justify-center mb-4">
@@ -70,13 +109,13 @@ export const AccountingDashboard = () => {
         </div>
         <div className="grid grid-cols-2">
           <div>
-            <CustomTable data={incomesData} title={"Incomes"} />
+            <CustomTable data={incomes} title={"Incomes"} />
           </div>
           <div>
-            <CustomTable data={expensesData} title={"Expenses"}/>
+            <CustomTable data={expenses} title={"Expenses"} />
           </div>
         </div>
-        
+
       </div>
     </div>
   );
--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,37 +1,32 @@
-import React from 'react';
-
+//import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CEditableTable from "@/components/customs/c_EditableTable";
 import {ActionPanel} from "@/components/customs/c_actionPanel"
 
-const expenses = [
-  {
-    id: 1,
-    date: "2025-04-01",
-    amount: 222,
-    description: "Sale of goods",
-    name: "Department Store",
-    status: "Active"
-  },
-  {
-    id: 2,
-    date: "2025-04-02",
-    amount: 97,
-    description: "Consulting income",
-    name: "Consulting ABC",
-    status: "Active"
-  },
-  {
-    id: 3,
-    date: "2025-04-03",
-    amount: 167,
-    description: "Advertising income",
-    name: "Ad Agency XYZ",
-    status: "Cancelled"
-  },
 
-];
 
 export const Expenses = () => {
+
+  //Hook to save my data
+  const [expenses, setExpenses] = useState([]);
+
+    useEffect(() => {
+      getExpensesList();
+    }, []);
+
+      const getExpensesList = async () => {
+
+    try {
+      // Variable for wait the get response and then save it into categories useState hook
+      const response = await api.get('http://localhost:3000/v1/expenses/list');
+      setExpenses(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-4">Expenses</h1>
