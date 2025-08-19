0daddae0e00aa14b82aef8926497c714c80ee886
Manuel MM <mmmongecr@hotmail.com>
2025-08-19 15:39:01 -0600

    minor fix

--- src/components/customs/c_editableTable.jsx
+++ src/components/customs/c_editableTable.jsx
@@ -31,70 +31,80 @@ import { Pencil, Trash2 } from "lucide-react";
 
 
 const DynamicTable = ({ data, title }) => {
-  const headers = Object.keys(data[0]);
 
-  return (
+  if (data.lenght > 1) {
 
-    <div className="flex justify-center">
-      <Card>
-        <CardHeader>
-          <CardTitle><b>{title}</b></CardTitle>
-        </CardHeader>
-        <CardContent>
 
-          <Table className="rounded-[12px] w-[1250px]">
-            <TableHeader>
-              <TableRow>
-                {headers.map((header, index) => (
-                  <TableHead key={index} className="text-center">{header}</TableHead>
-                ))}
-                <TableHead className="w-[150px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data.map((row, rowIndex) => (
-                <TableRow key={rowIndex}>
-                  {headers.map((header, cellIndex) => (
-                    <TableCell key={cellIndex} className="text-center">{row[header]}</TableCell>
+
+    const headers = Object.keys(data[0]);
+
+    return (
+
+      <div className="flex justify-center">
+        <Card>
+          <CardHeader>
+            <CardTitle><b>{title}</b></CardTitle>
+          </CardHeader>
+          <CardContent>
+
+            <Table className="rounded-[12px] w-[1250px]">
+              <TableHeader>
+                <TableRow>
+                  {headers.map((header, index) => (
+                    <TableHead key={index} className="text-center">{header}</TableHead>
                   ))}
-                  <TableCell className="flex justify-end gap-2">
-                    <Button className="w-8 h-8">
-                      <Pencil />
-                    </Button>
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <Button className="w-8 h-8">
-                          <Trash2 />
-                        </Button>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-md">
-                        <DialogHeader>
-                          <DialogTitle>Delete Confirmation</DialogTitle>
-                          <DialogDescription>
-                            This action cannot be undone. This will permanently delete this provider from your records.
-                          </DialogDescription>
-                        </DialogHeader>
-                        <DialogFooter className="sm:justify-center">
-                          <DialogClose asChild>
-                            <Button>Confirm</Button>
-                          </DialogClose>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
-                  </TableCell>
+                  <TableHead className="w-[150px]"></TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+              <TableBody>
+                {data.map((row, rowIndex) => (
+                  <TableRow key={rowIndex}>
+                    {headers.map((header, cellIndex) => (
+                      <TableCell key={cellIndex} className="text-center">{row[header]}</TableCell>
+                    ))}
+                    <TableCell className="flex justify-end gap-2">
+                      <Button className="w-8 h-8">
+                        <Pencil />
+                      </Button>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="w-8 h-8">
+                            <Trash2 />
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-md">
+                          <DialogHeader>
+                            <DialogTitle>Delete Confirmation</DialogTitle>
+                            <DialogDescription>
+                              This action cannot be undone. This will permanently delete this provider from your records.
+                            </DialogDescription>
+                          </DialogHeader>
+                          <DialogFooter className="sm:justify-center">
+                            <DialogClose asChild>
+                              <Button>Confirm</Button>
+                            </DialogClose>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
 
-        </CardContent>
-      </Card>
-    </div>
+          </CardContent>
+        </Card>
+      </div>
 
 
 
 
-  );
+    );
+  } else {
+    return(
+      <h1 className="text-3xl font-bold mb-4">No se han encontrado resultados</h1>
+    );
+  }
 };
 
 export const EditableTable = ({ data, title }) => {
